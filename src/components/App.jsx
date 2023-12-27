533277f1018e5cde154f6623433a175ad3fdd144
Serhii1727 <kravchenko087@ukr.net>
2023-12-27 17:57:17 -0500

    change state to useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,60 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
 import Notification from './Notification';
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export function App() {
+  const [feedbackGood, setFeedbackGood] = useState(0);
+  const [feedbackNeutral, setFeedbackNeutral] = useState(0);
+  const [feedbackBad, setFeedbackBad] = useState(0);
+
+  const handleFeedback = name => {
+    if (name === 'good') {
+      setFeedbackGood(prevFeedbackGood => prevFeedbackGood + 1);
+    }
+    if (name === 'neutral') {
+      setFeedbackNeutral(prevFeedbackNeutral => prevFeedbackNeutral + 1);
+    }
+    if (name === 'bad') {
+      setFeedbackBad(prevFeedbackBad => prevFeedbackBad + 1);
+    }
   };
 
-  handleFeedback = name => {
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
+  const countTotalFeedback = () => {
+    return feedbackGood + feedbackNeutral + feedbackBad;
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((feedbackGood / countTotalFeedback()) * 100);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    return Math.round((good / this.countTotalFeedback()) * 100);
-  };
+  const optionsButton = ['good', 'neutral', 'bad'];
+  const totalFeedback = countTotalFeedback();
+  const positivePercentageFeedback = countPositiveFeedbackPercentage();
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const optionsButton = Object.keys(this.state);
-    const totalFeedback = this.countTotalFeedback();
-    const positivePercentageFeedback = this.countPositiveFeedbackPercentage();
-
-    return (
-      <div className={css.feedback__container}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={optionsButton}
-            onLeaveFeedback={this.handleFeedback}
+  return (
+    <div className={css.feedback__container}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={optionsButton}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+      {!totalFeedback ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title={'Statistics'}>
+          <Statistics
+            feedbackGood={feedbackGood}
+            feedbackNeutral={feedbackNeutral}
+            feedbackBad={feedbackBad}
+            total={totalFeedback}
+            positivePercentage={positivePercentageFeedback}
           />
         </Section>
-        {!totalFeedback ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Section title={'Statistics'}>
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback}
-              positivePercentage={positivePercentageFeedback}
-            />
-          </Section>
-        )}
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,22 +1,22 @@
 import css from './Statistics.module.css';
 
 export default function Statistics({
-  good,
-  neutral,
-  bad,
+  feedbackGood,
+  feedbackNeutral,
+  feedbackBad,
   total,
   positivePercentage,
 }) {
   return (
     <ul className={css.feedback__statistics}>
       <li className={css.feedback__item}>
-        <p>Good: {good}</p>
+        <p>Good: {feedbackGood}</p>
       </li>
       <li className={css.feedback__item}>
-        <p>Neutral: {neutral}</p>
+        <p>Neutral: {feedbackNeutral}</p>
       </li>
       <li className={css.feedback__item}>
-        <p>Bad: {bad}</p>
+        <p>Bad: {feedbackBad}</p>
       </li>
       <li className={css.feedback__item}>
         <p>Total: {total}</p>
